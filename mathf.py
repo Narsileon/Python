58083141f9c798650b65f8ee2b7321e57b7ef4ec
Narsilien <rigaux.jonathan@gmail.com>
2022-09-25 17:13:11 +0200

    Update

--- mathf.py
+++ mathf.py
@@ -16,3 +16,6 @@ def get_age(birthdate):
     today = date.today()
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return age
+
+def sum_range(start, end):
+    return (end * (end + 1) - start * (start - 1)) / 2
